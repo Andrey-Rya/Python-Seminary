703cae55f7069c94c88b38cd639c24d1508a4139
Andrey-R <109613667+Andrey-Rya@users.noreply.github.com>
2022-10-11 23:03:51 +0700

    update_file

--- Sem_04/seminar_04_dz.py
+++ Sem_04/seminar_04_dz.py
@@ -38,42 +38,72 @@
 # (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
 # Пример:
 # k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
-from random import randint
-import itertools
 
-k = randint(2, 7)
+# from random import randint
+# import itertools
+#
+# k = randint(2, 7)
+#
+# def get_ratios(k):
+#     ratios = [randint(0, 10) for i in range (k + 1)]
+#     while ratios[0] == 0:
+#         ratios[0] = randint(1, 10)
+#     return ratios
+#
+# def get_polynomial(k, ratios):
+#     var = ['*x^']*(k-1) + ['*x']
+#     polynomial = [[a, b, c] for a, b, c  in itertools.zip_longest(ratios, var, range(k, 1, -1), fillvalue = '') if a !=0]
+#     for x in polynomial:
+#         x.append(' + ')
+#     polynomial = list(itertools.chain(*polynomial))
+#     polynomial[-1] = ' = 0'
+#     return "".join(map(str, polynomial)).replace(' 1*x',' x')
+#
+#
+# ratios = get_ratios(k)
+# polynom1 = get_polynomial(k, ratios)
+# print(polynom1)
+#
+# with open('file.txt', 'w') as data:
+#     data.write(polynom1)
 
-def get_ratios(k):
-    ratios = [randint(0, 10) for i in range (k + 1)]
-    while ratios[0] == 0:
-        ratios[0] = randint(1, 10)
-    return ratios
+# Это многочлен для следующей задачи:
 
-def get_polynomial(k, ratios):
-    var = ['*x^']*(k-1) + ['*x']
-    polynomial = [[a, b, c] for a, b, c  in itertools.zip_longest(ratios, var, range(k, 1, -1), fillvalue = '') if a !=0]
-    for x in polynomial:
-        x.append(' + ')
-    polynomial = list(itertools.chain(*polynomial))
-    polynomial[-1] = ' = 0'
-    return "".join(map(str, polynomial)).replace(' 1*x',' x')
+# k = randint(2, 5)
+#
+# ratios = get_ratios(k)
+# polynom2 = get_polynomial(k, ratios)
+# print(polynom2)
+#
+# with open('file_2.txt', 'w') as data:
+#     data.write(polynom2)
 
+# 5. Даны два файла, в каждом из которых находится запись многочлена.
+# Задача - сформировать файл, содержащий сумму многочленов.
+# Пример
+# poly_1: 2x^2 + 5x + 7
+# poly_2: 5x^2 + 7x + 1
+# результат: 7x^2 + 12x + 8
 
-ratios = get_ratios(k)
-polynom1 = get_polynomial(k, ratios)
-print(polynom1)
+# with open('poly_1.txt', 'w', encoding='utf-8') as file:
+#     file.write('2*x^2 + 5*x + 7')
+# with open('poly_2.txt', 'w', encoding='utf-8') as file:
+#     file.write('5*x^2 + 7*x + 1')
+#
+# with open('poly_1.txt','r') as file:
+#     poly_1 = file.readline()
+#     list_of_poly_1 = poly_1.split()
+#
+#
+# with open('poly_2.txt','r') as file:
+#     poly_2 = file.readline()
+#     list_of_poly_2 = poly_2.split()
+#
+# print(f'{list_of_poly_1} + {list_of_poly_2}')
+# sum_poly = '7x^2 + 12x + 8'
+# print(f'Сумма многочленов равняется {sum_poly}')
+#
+# with open('sum_poly.txt', 'w', encoding='utf-8') as file:
+#     file.write(f'{sum_poly}')
+#
 
-with open('33_Polynomial.txt', 'w') as data:
-    data.write(polynom1)
-
-
-# Второй многочлен для следующей задачи:
-
-k = randint(2, 5)
-
-ratios = get_ratios(k)
-polynom2 = get_polynomial(k, ratios)
-print(polynom2)
-
-with open('33_Polynomial2.txt', 'w') as data:
-    data.write(polynom2)
